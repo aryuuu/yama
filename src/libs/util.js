5d0ad98f5423ce16c5cbf47cda56702daac6cc18
M Algah Fattah Illahi <herppratama@gmail.com>
2021-07-29 13:19:16 +0700

    add util functions

---
+++ src/libs/util.js
@@ -0,0 +1,13 @@
+export const bufToHex = (buffer) => {
+  var s = '', h = '0123456789ABCDEF';
+  (new Uint8Array(buffer)).forEach((v) => { s += h[v >> 4] + h[v & 15]; });
+  s = '0x' + s;
+  return s;
+}
+
+export const hexToBuf = (hex) => {
+   // remove the leading 0x
+  hex = hex.replace(/^0x/, '');
+  const typedArray = new Uint8Array(hex.match(/[\da-f]{2}/gi).map((h) => parseInt(h, 16)))
+  return typedArray.buffer
+}
